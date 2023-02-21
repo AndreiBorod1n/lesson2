90c7a5b3de5fb63b78238e167d260183cda7ba15
Andrei Borodin <124201108+AndreiBorod1n@users.noreply.github.com>
2023-02-21 20:42:22 +0500

    Add files via upload

---
+++ Example003.cs
@@ -0,0 +1,22 @@
+﻿int [] array = {13, 25, 38, 42, 51, 66, 73, 89};
+
+int n = array.Length;
+int find = 73;
+
+int index = 0;
+
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    
+    //index = index + 1;
+    index ++;
+}
+
+
+
+ 
