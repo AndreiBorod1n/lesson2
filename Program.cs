f560967b6c512a000a01d6e7173569a3aae56d07
Andrei Borodin <124201108+AndreiBorod1n@users.noreply.github.com>
2023-02-21 20:41:11 +0500

    Add example002

--- Program.cs
+++ Program.cs
@@ -5,25 +5,13 @@
     if (arg3 > result) result = arg3;
     return result;
 }
+//             0   1   2   3   4  5   6   7   8
+int[] array = {16, 28, 31, 42, 5, 65, 71, 82, 93};
 
-int a1 = 23;
-int b1 = 12;
-int c1 = 31;
-int a2 = 22;
-int b2 = 51;
-int c2 = 14;
-int a3 = 54;
-int b3 = 850;
-int c3 = 422;
-
-// int max1 = Max (a1,b1,c1);
-// int max2 = Max (a2,b2,c2);
-// int max3 = Max (a3,b3,c3);
-// int max = Max (max1,max2,max3);
-
-int max = Max (
-    Max (a1,b1,c1),
-    Max (a2,b2,c2),
-    Max (a3,b3,c3));
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 
 Console.WriteLine(max);
